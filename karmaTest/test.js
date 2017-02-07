82cc0ad918d54de167b87a7db5f45d315848d06f
hualy <784975128@qq.com>
2017-02-08 00:01:41 +0800

    添加内容

---
+++ karmaTest/test.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/2/7.
+ */
+describe("A suite of basic functions", function() {
+    it("reverse word",function(){
+        expect(add(1,3)).toEqual(4);
+    });
+});
